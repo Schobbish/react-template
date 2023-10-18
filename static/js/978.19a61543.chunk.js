8052acf40a64d5036b28cd0211597087e6b3b631
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2023-10-18 16:33:21 -0500

    Updates

---
+++ static/js/978.19a61543.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[978],{3978:function(e,t,r){r.r(t),r.d(t,{worker:function(){return l}});var n=r(8509),u=n.rest.get("/api/hello_world",(function(e,t,r){return t(r.status(200),r.json("hello,\nworld!"))})),l=(0,n.setupWorker)(u)}}]);
+//# sourceMappingURL=978.19a61543.chunk.js.map
