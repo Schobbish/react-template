5ba74df70a48845090fa47b6b44c60bb0b79f82c
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2023-11-10 01:06:27 -0600

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 
 /**
- * For now it's here for just ScrollRestoration but this is a
+ * For now it's just here for ScrollRestoration but this is a
  * good place for global context and other things as well.
  */
 export function App() {
