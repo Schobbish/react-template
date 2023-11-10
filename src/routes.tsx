7622f2863855cc5e3f3170fca0daae39b86a655e
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2023-11-10 01:43:54 -0600

    add comment

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { Error404 } from "./pages/Error404";
 import { Home } from "./pages/Home";
 
+/** React Router routes for this app. */
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
